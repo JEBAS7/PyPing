9557f95b95c5b40038599d380b051e67b201f7b6
JEBAS7 <jebas7@outlook.com>
2026-08-09 19:47:07 -0300

    Pyping configurado para o game Black desert online SA

--- pyping.py
+++ pyping.py
@@ -33,7 +33,7 @@ class PingOverlay:
         self.root.config(bg="black")
         self.root.attributes("-transparentcolor", "black") # Torna o fundo preto transparente
         
-        # Posição inicial na tela (X=20, Y=20) - Altere se quiser mover o padrão
+        # Posição inicial na tela (X=1500, Y=50) - Altere se quiser mover o padrão
         self.root.geometry("+1500+50")
         
         # Texto do Ping (Verde limão para destacar no jogo)
